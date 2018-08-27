9d248d33fe18018befd8bfaafc661bf4f0515f90
jaimesalas <jaime.salas@lemoncode.net>
2018-08-27 19:42:14 +0200

    remove third party components from branch

--- material-sandbox-angular/src/app/components/components.module.ts
+++ material-sandbox-angular/src/app/components/components.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ThirdPartyComponentsModule } from '../third-party-components/third-party-components.module';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CarouselItemComponent } from './carousel-item/carousel-item.component';
 import { DynamicCarouselItemAnchorDirective } from './dynamic-carousel-item-anchor.directive';
 
 @NgModule({
   imports: [
-    CommonModule,
-    ThirdPartyComponentsModule
+    CommonModule
   ],
   declarations: [
     CarouselComponent,
